e99bbaecc3ff7951f4c1ac7b2e5ddb167dda5af1
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 00:15:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,15 @@
 require 'pry'
-def dictionary 
+def dictionary
 {
 "hello" => 'hi',
 "to" =>' 2',
 "two" => '2',
-"too" => '2', 
+"too" => '2',
 "for" => '4',
 "four" => '4',
 "be" => 'b',
 "you" => 'u',
-"at" => '@', 
+"at" => '@',
 "and" => '&'
 }
 end
