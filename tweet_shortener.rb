f4fd0c48eae3240ed32dff8d3cdbf115edca0d51
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 01:19:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,35 +1,21 @@
 require 'pry'
 
-
+dictionary =
+{
+"hello" => 'hi',
+"to" =>' 2',
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+"be" => 'b',
+"you" => 'u',
+"at" => '@',
+"and" => '&'
+}
 
 def word_substituter(tweet)
-  dictionary =
-  {
-  "hello" => 'hi',
-  "to" =>' 2',
-  "two" => '2',
-  "too" => '2',
-  "for" => '4',
-  "four" => '4',
-  "be" => 'b',
-  "you" => 'u',
-  "at" => '@',
-  "and" => '&'
-  }
 
-tweet.split.collect { |word| dictionary.keys.include?(word.downcase) ? 
-    word = dictionary[word.downcase] : word }
+  tweet.split {|word| dictionary.keys.include?(word.downcase) ? 
+    word = dictionary[word.downcase] : word}
       end
-
-
-# tweet_array = tweet.split
-# tweet_array.each do |i|
-#     if dictionary.keys.include?(i)
-#     dictionary.each do |key, value|
-#       if i == key
-#
-#       end
-#     end
-#   end
-#
-# end
