04f4828c8f60263944df9147aa206e18aec03470
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 00:15:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+def dictionary 
+{
+"hello" => 'hi',
+"to" =>' 2',
+"two" => '2',
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+"be" => 'b',
+"you" => 'u',
+"at" => '@', 
+"and" => '&'
+}
+end
+
+def word_substituter(tweet)
+tweet_array = []
+
+tweet.each do |i|
+  tweet_array << i
+end
+binding.pry
+end
